a5e6b5546900f2bfd008a26829951753dcfc1e58
Smit Shah <smitshah1999@gmail.com>
2023-11-11 14:27:49 -0600

    working extension for content scripts

--- content.js
+++ content.js
@@ -3,13 +3,12 @@ let currentUrl = window.location.href;
 let greenHouseRegex = /https:\/\/boards.greenhouse.io\/*/;
 let submitButton;
 let position;
-let time;//This is going 
+let time; //This is going
 // Make the button correspond to the submit button
-if(greenHouseRegex.test(currentUrl)){
-    submitButton = document.querySelector('input[value="Submit Application"]')
-    position = document.querySelector('h1').innerHTML
+if (greenHouseRegex.test(currentUrl)) {
+  submitButton = document.querySelector('input[value="Submit Application"]');
+  position = document.querySelector("h1").innerHTML;
 }
-submitButton.addEventListener('click', function() {
-    time = Date.now()
+submitButton.addEventListener("click", function () {
+  time = Date.now();
 });
-
