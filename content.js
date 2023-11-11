ba06c940749229eea867305386f485010b6a327d
Blackcamel887 <Pajamas887.com>
2023-11-11 14:11:31 -0600

    fix content.js

--- content.js
+++ content.js
@@ -1,7 +1,15 @@
 let currentUrl = window.location.href;
+// The regexes for different websites
 let greenHouseRegex = /https:\/\/boards.greenhouse.io\/*/;
 let submitButton;
+let position;
+let time;//This is going 
+// Make the button correspond to the submit button
 if(greenHouseRegex.test(currentUrl)){
-    submitButton = 
+    submitButton = document.querySelector('input[value="Submit Application"]')
+    position = document.querySelector('h1').innerHTML
 }
-
+submitButton.addEventListener('click', function() {
+    time = Date.now()
+    if 
+});
