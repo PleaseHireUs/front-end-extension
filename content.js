451688c61d8daff146144cce8c220e16879d32fa
Smit Shah <smitshah1999@gmail.com>
2023-11-11 14:01:39 -0600

    add skeleton to run the extension

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 let currentUrl = window.location.href;
 let greenHouseRegex = /https:\/\/boards.greenhouse.io\/*/;
 let submitButton;
-if(greenHouseRegex.test(currentUrl)){
-    submitButton = 
+if (greenHouseRegex.test(currentUrl)) {
+  submitButton = document.querySelector();
 }
-
