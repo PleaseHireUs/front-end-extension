ba79d1752f9e8d792d3bd340b0a2dc88f3a950d2
Blackcamel887 <Pajamas887.com>
2023-11-11 14:53:28 -0600

    get all information correctly

--- content.js
+++ content.js
@@ -3,13 +3,23 @@ let currentUrl = window.location.href;
 let greenHouseRegex = /https:\/\/boards.greenhouse.io\/*/;
 let submitButton;
 let position;
-let time;//This is going 
+let time;//This is going to be encoded in a number
+let company;
 // Make the button correspond to the submit button
 if(greenHouseRegex.test(currentUrl)){
-    submitButton = document.querySelector('input[value="Submit Application"]')
-    position = document.querySelector('h1').innerHTML
+    submitButton = document.querySelector('input[value="Submit Application"]');
+    position = document.querySelector('h1').innerHTML;
+    company = document.querySelector('span[class="company-name"]').innerHTML.trim().substring(3)
 }
 submitButton.addEventListener('click', function() {
-    time = Date.now()
+    time = Date.now();
+    let jsonObject = {
+        "url":currentUrl,
+        "position":position,
+        "time": time,
+        "company": company
+    };
+    let result = JSON.stringify(jsonObject);
+    console.log(result)
 });
 
